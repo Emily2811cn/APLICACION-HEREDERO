9229d6955e71ff6d45bbc23c95f0ceabd83d4fab
Emily <emcruznarva28@gmail.com>
2025-12-16 21:58:31 -0500

    modificaciones de diseño

--- src/app/componentes/header-global/header-global.component.ts
+++ src/app/componentes/header-global/header-global.component.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { IonHeader, IonTitle, IonToolbar, IonSearchbar } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonSearchbar } from '@ionic/angular/standalone';
+import { IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-header-global',
   templateUrl: './header-global.component.html',
   styleUrls: ['./header-global.component.scss'],
   standalone: true,
-  imports: [IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonSearchbar]
+  imports: [IonHeader, IonToolbar, CommonModule, FormsModule, IonSearchbar],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HeaderGlobalComponent implements OnInit {
 
