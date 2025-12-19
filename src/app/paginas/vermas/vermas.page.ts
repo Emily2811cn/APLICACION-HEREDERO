d590ef771a1f3c426404b2351693c54b2c110208
Emily <emcruznarva28@gmail.com>
2025-12-18 23:39:20 -0500

    clase 18 de diciembre

--- src/app/paginas/vermas/vermas.page.ts
+++ src/app/paginas/vermas/vermas.page.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonButton } from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
 import { HeaderGlobalComponent } from 'src/app/componentes/header-global/header-global.component';
+import { FooterGlobalComponent } from 'src/app/componentes/footer-global/footer-global.component';
 
 @Component({
   selector: 'app-vermas',
   templateUrl: './vermas.page.html',
   styleUrls: ['./vermas.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, CommonModule, FormsModule, HeaderGlobalComponent ]
+  imports: [IonContent, IonHeader, CommonModule, FormsModule, HeaderGlobalComponent, IonButton, FooterGlobalComponent]
 })
 export class VermasPage implements OnInit {
 producto: any={};
